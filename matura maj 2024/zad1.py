7f36a3463d2717a2a5aa54fb624fdac460a51b65
6tab9 <tabolator1@gmail.com>
2025-11-14 15:57:47 +0100

    matura 2024

---
+++ matura maj 2024/zad1.py
@@ -0,0 +1,13 @@
+#Zad 1
+a = [
+    [0,0,0],
+    [0,0,0],
+    [0,0,0]
+    ]
+p = []
+n = len(a[0])
+m = len(a)
+for i in range(0,n):
+    for j in range(0,m):
+        if a[i][j]= 0:
+            p
---
+++ matura maj 2024/zad2.py
@@ -0,0 +1,15 @@
+#Zad 2
+n = int(input("Podaj n: "))
+b = 1
+c = 0
+count = 0
+while n>0:
+    a = n % 10
+    n = n // 10
+    if a % 2 == 0:
+        c = c + b * (a // 2)
+    else:
+        c = c + b
+        count += 1
+    b *= 10
+print(c, count)
